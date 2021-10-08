1bacd789287aeb0002bba7ff5ae0ddcaf91c3d8e
KenAprillio <josiahken@gmail.com>
2021-10-08 15:44:49 +0700

    Attacker Script Update

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data
+{
+    public static float coin = 300;
+    public static bool isGameOver = false;
+    public static bool isComplete = false;
+}
